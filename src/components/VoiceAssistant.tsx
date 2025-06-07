86f51d36bf080a39bd6aa22023bcbfb1c388d1e0
M.Homnaadda <chanti20017@gmail.com>
2025-06-07 11:54:27 +0530

    Updated VoiceAssistant.tsx

--- src/components/VoiceAssistant.tsx
+++ src/components/VoiceAssistant.tsx
@@ -507,7 +507,7 @@ const VoiceAssistant = () => {
                 {isListening && (
                   <div className="flex items-center gap-4">
                     <div className="flex space-x-2">
-                      <div className="w-3 h-3 bg-blue-400 rounded-full animate-bounce" style={{ animationDelay: '0ms' }}></div>
+                      <div className="w-3 h-3 bg-blue-400 rounded-full animate-bounce\" style={{ animationDelay: '0ms' }}></div>
                       <div className="w-3 h-3 bg-blue-400 rounded-full animate-bounce" style={{ animationDelay: '150ms' }}></div>
                       <div className="w-3 h-3 bg-blue-400 rounded-full animate-bounce" style={{ animationDelay: '300ms' }}></div>
                     </div>
